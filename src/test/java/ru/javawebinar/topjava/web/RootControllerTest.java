df2512ed3d670468f3c271a33fe23be24a59caaa
Sergei <kserg.ru@mail.ru>
2023-12-02 13:13:36 +0200

    HW07_hometask_1.1

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static ru.javawebinar.topjava.UserTestData.*;
+import static ru.javawebinar.topjava.MealTestData.*;
 
 class RootControllerTest extends AbstractControllerTest {
 
@@ -28,5 +30,23 @@ class RootControllerTest extends AbstractControllerTest {
                             }
                         }
                 ));
+    }
+
+    @Test
+    void getMeals() throws Exception {
+        perform(get("/meals"))
+                .andDo(print())
+                .andExpect(model().attributeDoesNotExist("excess"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals",
+                        new AssertionMatcher<List<Meal>>() {
+                            @Override
+                            public void assertion(List<Meal> actual) throws AssertionError {
+                                MEAL_MATCHER.assertMatch(actual, meal7, meal6, meal5, meal4, meal3, meal2, meal1);
+                            }
+                        }
+                        ));
     }
 }
